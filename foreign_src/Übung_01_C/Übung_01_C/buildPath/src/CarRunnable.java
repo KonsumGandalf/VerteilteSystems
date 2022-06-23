56e892b9e1a4960330a8fbc9272efd2f293b31d4
David2 Schmidt <scd48410@hs-regensburg.de>
2022-06-23 13:16:34 +0200

    othUpdate

---
+++ foreign_src/Übung_01_C/Übung_01_C/buildPath/src/CarRunnable.java
@@ -0,0 +1,13 @@
+package src;
+
+public class carRunnable implements Runnable{
+    private int customerId;
+
+    carRunnable(int customerId){
+        this.customerId = customerId
+    }
+
+    public void buy() throws {
+
+    }
+}
